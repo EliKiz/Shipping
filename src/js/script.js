e6e4e0a7713a69f5ba917c51fde3bca3b7d30f13
Ilya <ilia0806280@gmail.com>
2022-03-07 23:03:22 +0400

    Finish last section and start adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,40 @@ document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next'); 
 });
 
+$(document).ready(function(){
+    function validateForms(form){
+     $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: {
+                required: true,
+                minlength: 4,
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуста введите свое имя",
+                minlength: jQuery.validator.format("Введите минимум {0} символа!!!")
+              },
+            phone: {
+                required: "Пожалуста введите номер телефона",
+                minlength: jQuery.validator.format("Введите минимум {0} символа!!!")
+            },
+            email: {
+              required: "Пожалуста введите свой email",
+              email: "Пример name@domain.com"
+            }
+        }
+    });
+};
+validateForms('#consultation-form');
+validateForms('#contacts-form');
+
+});
